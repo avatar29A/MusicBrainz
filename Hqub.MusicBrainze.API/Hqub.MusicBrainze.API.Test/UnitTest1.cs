8c5fba46a36abba3f78500255d807c29350f3d83
Boris Glebov <boris.glebov@hotmail.com>
2012-12-05 01:21:48 +0600

    add projects

---
+++ Hqub.MusicBrainze.API/Hqub.MusicBrainze.API.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hqub.MusicBrainze.API.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
