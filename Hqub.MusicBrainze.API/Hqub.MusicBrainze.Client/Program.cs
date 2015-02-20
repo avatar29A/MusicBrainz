4e89e752ae2c019290fd17f583b4f34fd83da339
Glebov Boris <avatar29A@gmail.com>
2015-02-20 19:41:23 +0500

    Update Client project. Add new samples

--- Hqub.MusicBrainze.API/Hqub.MusicBrainze.Client/Program.cs
+++ Hqub.MusicBrainze.API/Hqub.MusicBrainze.Client/Program.cs
@@ -13,11 +13,73 @@ namespace Hqub.MusicBrainz.Client
     {
         private static void Main(string[] args)
         {
-            Test();
+            ShowTracksByAlbum("Король и Шут", "Акустический альбом");
+//            ShowAlbumsTracksByArtist("Король и Шут");
 
-            Console.WriteLine("OK");
             Console.ReadKey();
         }
+
+        private static Artist GetArtist(string name)
+        {
+            var artists = Artist.Search(name);
+
+            var artist = artists.First();
+            Console.WriteLine(artist.Name);
+
+            return artist;
+        }
+
+        private static async void ShowTracksByAlbum(string artistName, string albumName)
+        {
+            var artist = GetArtist(artistName);
+
+            var albums = await Release.BrowseAsync("artist", artist.Id);
+
+
+            var album = albums.FirstOrDefault(r => r.Title.ToLower() == albumName.ToLower());
+
+            if (album == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Album not found.\n");
+                Console.ResetColor();
+
+                Console.WriteLine("Results:\n");
+                foreach (var al in albums)
+                {
+                    Console.WriteLine("\t{0}", al.Title);
+                }
+
+                return;
+            }
+
+            Console.WriteLine("\t{0}", albumName);
+
+            var tracks = await Recording.BrowseAsync("release", album.Id, 100);
+            foreach (var track in tracks)
+            {
+                Console.WriteLine("\t\t{0}", track.Title);
+            }
+        }
+
+        private static async void ShowAlbumsTracksByArtist(string name)
+        {
+            var artist = GetArtist(name);
+
+            var releases = await Release.BrowseAsync("artist", artist.Id, 100, 0, "media");
+
+            foreach (var release in releases)
+            {
+                Console.WriteLine("\t{0}", release.Title);
+                var tracks = await Recording.BrowseAsync("release", release.Id, 100);
+
+                foreach (var track in tracks)
+                {
+                    Console.WriteLine("\t\t{0}", track.Title);
+                }
+            }
+        }
+
 
         private static async void Test()
         {
