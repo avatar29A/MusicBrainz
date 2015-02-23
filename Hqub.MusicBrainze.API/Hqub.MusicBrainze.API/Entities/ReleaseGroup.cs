2dd4a9c367d413d09558055d434d24bac8fac8dc
wo80 <christian.woltering@tu-dortmund.de>
2015-02-23 15:27:43 +0100

    Add Get and Search methods to release groups.

--- Hqub.MusicBrainze.API/Hqub.MusicBrainze.API/Entities/ReleaseGroup.cs
+++ Hqub.MusicBrainze.API/Hqub.MusicBrainze.API/Entities/ReleaseGroup.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Hqub.MusicBrainz.API.Entities.Collections;
+using Hqub.MusicBrainz.API.Entities.Metadata;
 
 namespace Hqub.MusicBrainz.API.Entities
 {
@@ -11,6 +11,10 @@ namespace Hqub.MusicBrainz.API.Entities
     [XmlRoot("release-group", Namespace = "http://musicbrainz.org/ns/mmd-2.0#")]
     public class ReleaseGroup : Entity
     {
+        public const string EntityName = "release-group";
+
+        #region Properties
+
         [XmlAttribute("type")]
         public string ReleaseGroupType { get; set; }
 
@@ -32,5 +36,47 @@ namespace Hqub.MusicBrainz.API.Entities
         [XmlArray("tag-list")]
         [XmlArrayItem("tag")]
         public TagList Tags { get; set; }
+
+        #endregion
+
+        #region Subqueries
+
+        [XmlArray("artist-credit")]
+        [XmlArrayItem("name-credit")]
+        public List<NameCredit> Credits { get; set; }
+
+        [XmlArray("release-list")]
+        [XmlArrayItem("release")]
+        public List<Release> Releases { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        [Obsolete("Use GetAsync() method.")]
+        public static ReleaseGroup Get(string id, params string[] inc)
+        {
+            return GetAsync<ReleaseGroup>(EntityName, id, inc).Result;
+        }
+
+        [Obsolete("Use SearchAsync() method.")]
+        public static ReleaseGroupList Search(string query, int limit = 25, int offset = 0)
+        {
+            return SearchAsync<ReleaseGroupMetadataWrapper>(EntityName,
+                query, limit, offset).Result.Collection;
+        }
+
+        public async static Task<ReleaseGroup> GetAsync(string id, params string[] inc)
+        {
+            return await GetAsync<ReleaseGroup>(EntityName, id, inc);
+        }
+
+        public async static Task<ReleaseGroupList> SearchAsync(string query, int limit = 25, int offset = 0)
+        {
+            return (await SearchAsync<ReleaseGroupMetadataWrapper>(EntityName,
+                query, limit, offset)).Collection;
+        }
+
+        #endregion
     }
 }
