9bc0f17cffea7e815ae830357548414f4f86f5ce
Glebov Boris <avatar29A@gmail.com>
2015-02-26 20:03:53 +0500

    Rename directory names MusicBrainze -> MusicBrainz.
    Rename directory Portable version and change all path.

---
+++ Hqub.MusicBrainze.API/Hqub.MusicBrainz.API/Entities/Entity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Hqub.MusicBrainz.API.Entities.Collections;
+
+namespace Hqub.MusicBrainz.API.Entities
+{
+    public abstract class Entity
+    {
+        // TODO: remove "Raw" property (no need to waste memory)
+        public XElement Raw { get; set; }
+
+        public virtual void SetSchema(XElement schema)
+        {
+            //Raw = schema;
+        }
+
+        private static string CreateIncludeQuery(string[] inc)
+        {
+            return string.Join("+", inc);
+        }
+
+        protected async static Task<T> GetAsync<T>(string entity, string id, params string[] inc) where T : Entity
+        {
+            if (string.IsNullOrEmpty(entity))
+            {
+                throw new ArgumentException(string.Format(Resources.Messages.MissingParameter, "entity"));
+            }
+
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException(string.Format(Resources.Messages.MissingParameter, "id"));
+            }
+
+            return await WebRequestHelper.GetAsync<T>(WebRequestHelper.CreateLookupUrl(entity, id, CreateIncludeQuery(inc)));
+        }
+
+        protected async static Task<T> SearchAsync<T>(string entity, string query, int limit = 25, int offset = 0) where T : MetadataWrapper
+        {
+            if (string.IsNullOrEmpty(entity))
+            {
+                throw new ArgumentException(string.Format(Resources.Messages.MissingParameter, "entity"));
+            }
+
+            if (string.IsNullOrEmpty(query))
+            {
+                throw new ArgumentException(string.Format(Resources.Messages.MissingParameter, "query"));
+            }
+
+            return await WebRequestHelper.GetAsync<T>(WebRequestHelper.CreateSearchTemplate(entity,
+                query, limit, offset), withoutMetadata: false);
+        }
+
+        protected async static Task<T> BrowseAsync<T>(string entity, string relatedEntity, string relatedEntityId, int limit, int offset, params  string[] inc) where T : Entity
+        {
+            if (string.IsNullOrEmpty(entity))
+            {
+                throw new ArgumentException(string.Format(Resources.Messages.MissingParameter, "entity"));
+            }
+
+            return await WebRequestHelper.GetAsync<T>(WebRequestHelper.CreateBrowseTemplate(entity,
+                relatedEntity, relatedEntityId, limit, offset, CreateIncludeQuery(inc)), withoutMetadata: false);
+        }
+    }
+}
---
+++ Hqub.MusicBrainze.API/Hqub.MusicBrainz.API/Entities/Release.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Hqub.MusicBrainz.API.Entities.Collections;
+using Hqub.MusicBrainz.API.Entities.Metadata;
+
+namespace Hqub.MusicBrainz.API.Entities
+{
+    [XmlType(Namespace = "http://musicbrainz.org/ns/mmd-2.0#")]
+    [XmlRoot("release", Namespace = "http://musicbrainz.org/ns/mmd-2.0#")]
+    public class Release : Entity
+    {
+        public const string EntityName = "release";
+
+        #region Properties
+
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("status")]
+        public string Status { get; set; }
+
+        [XmlElement("quality")]
+        public string Quality { get; set; }
+
+        [XmlElement("text-representation")]
+        public TextRepresentation TextRepresentation { get; set; }
+
+        [XmlElement("date")]
+        public string Date { get; set; }
+
+        [XmlElement("country")]
+        public string Country { get; set; }
+
+        [XmlElement("barcode")]
+        public string Barcode { get; set; }
+
+        [XmlElement("release-group")]
+        public ReleaseGroup ReleaseGroup { get; set; }
+
+        [XmlElement("cover-art-archive")]
+        public CoverArtArchive CoverArtArchive { get; set; }
+
+        #endregion
+
+        #region Subqueries
+
+        [XmlArray("artist-credit")]
+        [XmlArrayItem("name-credit")]
+        public List<NameCredit> Credits { get; set; }
+
+        [XmlArray("label-info-list")]
+        [XmlArrayItem("label-info")]
+        public List<LabelInfo> Labels { get; set; }
+
+        [XmlElement("medium-list")]
+        public Collections.MediumList MediumList { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        [Obsolete("Use GetAsync() method.")]
+        public static Release Get(string id, params string[] inc)
+        {
+            return GetAsync<Release>(EntityName, id, inc).Result;
+        }
+
+        [Obsolete("Use SearchAsync() method.")]
+        public static ReleaseList Search(string query, int limit = 25, int offset = 0)
+        {
+            return SearchAsync<ReleaseMetadataWrapper>(EntityName,
+                query, limit, offset).Result.Collection;
+        }
+
+        public async static Task<Release> GetAsync(string id, params string[] inc)
+        {
+            return await GetAsync<Release>(EntityName, id, inc);
+        }
+
+        public async static Task<ReleaseList> SearchAsync(string query, int limit = 25, int offset = 0)
+        {
+            return (await SearchAsync<ReleaseMetadataWrapper>(EntityName,
+                query, limit, offset)).Collection;
+        }
+
+        public async static Task<ReleaseList> SearchAsync(QueryParameters<Release> query, int limit = 25, int offset = 0)
+        {
+            return (await SearchAsync<ReleaseMetadataWrapper>(EntityName,
+                query.ToString(), limit, offset)).Collection;
+        }
+
+        public static async Task<ReleaseList> BrowseAsync(string relatedEntity, string value, int limit = 25,
+            int offset = 0, params string[] inc)
+        {
+            return (await BrowseAsync<ReleaseMetadataWrapper>(EntityName,
+                relatedEntity, value, limit, offset, inc)).Collection;
+        }
+
+        #endregion
+    }
+
+    [XmlType(Namespace = "http://musicbrainz.org/ns/mmd-2.0#")]
+    [XmlRoot("text-representation", Namespace = "http://musicbrainz.org/ns/mmd-2.0#")]
+    public class TextRepresentation
+    {
+        [XmlElement("language")]
+        public string Language { get; set; }
+
+        [XmlElement("script")]
+        public string Script { get; set; }
+    }
+}
