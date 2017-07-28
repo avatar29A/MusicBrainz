44e91f4a1da0a24800980c820b8de94ec5cbd422
wo80 <christian.woltering@tu-dortmund.de>
2017-07-29 00:49:24 +0200

    Update portable project.

--- Hqub.MusicBrainz/Hqub.MusicBrainz.API.Portable/WebRequestHelper.cs
+++ Hqub.MusicBrainz/Hqub.MusicBrainz.API.Portable/WebRequestHelper.cs
@@ -1,74 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Hqub.MusicBrainz.API;
 using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace Hqub.MusicBrainz.API
 {
     internal static class WebRequestHelper
     {
-        public const string WebServiceUrl = "http://musicbrainz.org/ws/2/";
-        public const string LookupTemplate = "{0}/{1}/?inc={2}";
-        public const string BrowseTemplate = "{0}?{1}={2}&limit={3}&offset={4}&inc={5}";
-        public const string SearchTemplate = "{0}?query={1}&limit={2}&offset={3}";
+        private const string WebServiceUrl = "http://musicbrainz.org/ws/2/";
+        private const string LookupTemplate = "{0}/{1}/?inc={2}";
+        private const string BrowseTemplate = "{0}?{1}={2}&limit={3}&offset={4}&inc={5}";
+        private const string SearchTemplate = "{0}?query={1}&limit={2}&offset={3}";
 
-        internal static async Task<T> GetAsync<T>(string url, bool withoutMetadata = true) where T : Entities.Entity
+        private const string JsonFormat = "&fmt=json";
+
+
+        internal static async Task<T> GetAsync<T>(string url)
         {
-            MyHttpClient httpClient = new MyHttpClient(url);
-            HttpResponseMessage response = await httpClient.SendRequestAsync();
+            var client = new MyHttpClient(url);
+            HttpResponseMessage response = await client.SendRequestAsync();
 
             var responseBuffer = await response.Content.ReadAsStreamAsync();
             using (var stream = responseBuffer)
             {
-                var xml = XDocument.Load(stream);
-                var serializer = new XmlSerializer(typeof(T));
+                var serializer = new DataContractJsonSerializer(typeof(T));
 
-                //Add extension namespace:
-                var ns = new XmlSerializerNamespaces();
-                ns.Add("ext", "http://musicbrainz.org/ns/ext#-2.0");
-
-                //check valid xml schema:
-                if (xml.Root == null || xml.Root.Name.LocalName != "metadata")
-                    throw new NullReferenceException("Wrong Xml Format");
-
-                var node = withoutMetadata ? xml.Root.Elements().FirstOrDefault() : xml.Root;
-
-                if (node == null)
-                    return default(T);
-
-                return (T) serializer.Deserialize(node.CreateReader());
+                return (T)serializer.ReadObject(stream);
             }
         }
 
+        internal static string CreateIncludeQuery(string[] inc)
+        {
+            return string.Join("+", inc);
+        }
+
         /// <summary>
-        /// Lookup url query
+        /// Creates a webservice lookup template.
+        /// </summary>
+        internal static string CreateLookupUrl(string entity, string mbid, params string[] inc)
+        {
+            return CreateLookupUrl(entity, mbid, CreateIncludeQuery(inc));
+        }
+
+        /// <summary>
+        /// Creates a webservice lookup template.
         /// </summary>
         internal static string CreateLookupUrl(string entity, string mbid, string inc)
         {
-            return string.Format("{0}{1}", WebServiceUrl, String.Format(LookupTemplate, entity, mbid, inc));
+            return string.Format("{0}{1}{2}", WebServiceUrl,
+                string.Format(LookupTemplate, entity, mbid, inc), JsonFormat);
         }
 
         /// <summary>
-        /// Browse url query
+        /// Creates a webservice browse template.
+        /// </summary>
+        internal static string CreateBrowseTemplate(string entity, string relatedEntity, string mbid, int limit, int offset, params string[] inc)
+        {
+            return CreateBrowseTemplate(entity, relatedEntity, mbid, limit, offset, CreateIncludeQuery(inc));
+        }
+
+        /// <summary>
+        /// Creates a webservice browse template.
         /// </summary>
         internal static string CreateBrowseTemplate(string entity, string relatedEntity, string mbid, int limit, int offset, string inc)
         {
-            return string.Format("{0}{1}", WebServiceUrl, String.Format(BrowseTemplate, entity, relatedEntity, mbid, limit, offset, inc));
+            return string.Format("{0}{1}{2}", WebServiceUrl,
+                string.Format(BrowseTemplate, entity, relatedEntity, mbid, limit, offset, inc), JsonFormat);
         }
 
         /// <summary>
-        /// SearchTemplate
+        /// Creates a webservice search template.
         /// </summary>
         internal static string CreateSearchTemplate(string entity, string query, int limit, int offset)
         {
-            return string.Format("{0}{1}", WebServiceUrl, string.Format(SearchTemplate, entity, query, limit, offset));
+            query = Uri.EscapeUriString(query);
+
+            return string.Format("{0}{1}{2}", WebServiceUrl,
+                string.Format(SearchTemplate, entity, query, limit, offset), JsonFormat);
         }
     }
 }
