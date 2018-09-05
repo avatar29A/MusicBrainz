24428a2d17dd18d133dc375830296a393a799740
wo80 <christian.woltering@tu-dortmund.de>
2018-09-05 10:56:32 +0200

    Proper implementation of WebServiceException.

--- Hqub.MusicBrainz/Hqub.MusicBrainz.API/WebServiceException.cs
+++ Hqub.MusicBrainz/Hqub.MusicBrainz.API/WebServiceException.cs
@@ -3,12 +3,13 @@ namespace Hqub.MusicBrainz.API
 {
     using System;
     using System.Net;
+    using System.Runtime.Serialization;
 
     /// <summary>
     /// Exception containing error information returned by the MusicBrainz webservice.
     /// </summary>
     [Serializable]
-    public class WebServiceException : Exception
+    public sealed class WebServiceException : Exception
     {
         /// <summary>
         /// Gets the response HTTP status code.
@@ -19,6 +20,29 @@ namespace Hqub.MusicBrainz.API
         /// Gets the requested uri.
         /// </summary>
         public readonly string Uri;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebServiceException"/> class.
+        /// </summary>
+        public WebServiceException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebServiceException"/> class.
+        /// </summary>
+        public WebServiceException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebServiceException"/> class.
+        /// </summary>
+        public WebServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WebServiceException"/> class.
@@ -31,6 +55,32 @@ namespace Hqub.MusicBrainz.API
         {
             StatusCode = statusCode;
             Uri = uri;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebServiceException"/> class.
+        /// </summary>
+        private WebServiceException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            this.StatusCode = (HttpStatusCode)info.GetValue("StatusCode", typeof(HttpStatusCode));
+            this.Uri = info.GetString("Uri");
+        }
+
+        /// <summary>
+        /// Sets the SerializationInfo with information about the exception.
+        /// </summary>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            info.AddValue("StatusCode", StatusCode);
+            info.AddValue("Uri", Uri);
+
+            base.GetObjectData(info, context);
         }
     }
 }
