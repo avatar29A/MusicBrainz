0cd32d12324ff686f4994ada251d27e5ffb26c81
wo80 <christian.woltering@tu-dortmund.de>
2021-02-08 23:58:26 +0100

    Documentation fixes.

--- Hqub.MusicBrainz/Hqub.MusicBrainz.API/Cache/IRequestCache.cs
+++ Hqub.MusicBrainz/Hqub.MusicBrainz.API/Cache/IRequestCache.cs
@@ -12,11 +12,15 @@ namespace Hqub.MusicBrainz.API.Cache
         /// <summary>
         /// Add a request and its response to the cache.
         /// </summary>
+        /// <param name="request">The request url used to identify the cache item.</param>
+        /// <param name="response">The MusicbBrainz webservice response stream.</param>
         Task Add(string request, Stream response);
 
         /// <summary>
-        /// Add a request and its response to the cache.
+        /// Try to find the cached response for given request url.
         /// </summary>
+        /// <param name="request">The request url used to identify the cache item.</param>
+        /// <param name="stream">The response stream read form cache.</param>
         /// <returns>True, if a cache entry matching the request was found.</returns>
         Task<bool> TryGetCachedItem(string request, out Stream stream);
     }
