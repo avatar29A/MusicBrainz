21f779b3421e6f3262edfc933459951882974692
ashisharya01 <87526105+ashisharya01@users.noreply.github.com>
2023-08-24 19:57:18 +0530

    Update Constants.Designer.cs

--- Hqub.MusicBrainz/Hqub.MusicBrainz.API/Resources/Constants.Designer.cs
+++ Hqub.MusicBrainz/Hqub.MusicBrainz.API/Resources/Constants.Designer.cs
@@ -6,6 +6,7 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
+//
 //------------------------------------------------------------------------------
 
 namespace Hqub.MusicBrainz.API.Resources {
