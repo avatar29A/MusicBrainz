62ed4c0cb3346f0da4f9b4dbc17f644c81c5de2f
wo80 <christian.woltering@tu-dortmund.de>
2025-09-29 20:17:58 +0200

    Add sub-queries to Label class.

--- src/Hqub.MusicBrainz/Entities/Label.cs
+++ src/Hqub.MusicBrainz/Entities/Label.cs
@@ -80,6 +80,42 @@ namespace Hqub.MusicBrainz.Entities
         [DataMember(Name = "aliases")]
         public List<Alias> Aliases { get; set; }
 
+        /// <summary>
+        /// Gets or sets a list of releases associated to this release-group.
+        /// </summary>
+        /// <example>
+        /// var e = await client.Labels.GetAsync(mbid, "releases");
+        /// </example>
+        [DataMember(Name = "releases")]
+        public List<Release> Releases { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of tags associated to this release-group.
+        /// </summary>
+        /// <example>
+        /// var e = await client.Labels.GetAsync(mbid, "tags");
+        /// </example>
+        [DataMember(Name = "tags")]
+        public List<Tag> Tags { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of genres associated to this release-group.
+        /// </summary>
+        /// <example>
+        /// var e = await client.Labels.GetAsync(mbid, "genres");
+        /// </example>
+        [DataMember(Name = "genres")]
+        public List<Genre> Genres { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of relations associated to this artist.
+        /// </summary>
+        /// <example>
+        /// var e = await client.Labels.GetAsync(mbid, "url-rels");
+        /// </example>
+        [DataMember(Name = "relations")]
+        public List<Relation> Relations { get; set; }
+
         #endregion
     }
 }
